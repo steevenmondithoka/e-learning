8246f8f39d95d818dc563ac6214fda64bc3806fd
steevenmondithoka <steevemondithoka@gmail.com>
2025-04-06 13:08:39 +0530

    changed

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -17,7 +17,7 @@ try {
         case 'user.created':{
             const userData ={
                 _id:data.id,
-                email:data.email_addresses[0].email_adress,
+                email:data.email_addresses[0].email_address,
                 name:data.first_name+" "+data.last_name,
                 imageUrl:data.image_url,
             }
