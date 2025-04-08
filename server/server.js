54bae028c5e57b4158c79da49839a4e1350be01d
kavya <sahitya@gmail.com>
2025-04-08 22:40:36 +0530

    changed

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.use('/api/educator',express.json(),educatorRouter)
 
 app.use('/api/course',express.json(),courseRouter)
 app.use('/api/user',express.json(),userRouter)
-app.post('/stripe',express.raw({ type: 'application/json' }),stripeWebhooks)
+app.post("/stripe", bodyParser.raw({ type: 'application/json' }), stripeWebhooks);
 //port
 
 const PORT = process.env.PORT || 5000
